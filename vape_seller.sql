68bc49a04751e0f73c814ec1c7aba9046a11aafb
haroldjames22 <jamesharold.villena@gmail.com>
2025-05-26 15:01:24 +0800

    UPDATED MAY 26

--- vape_seller.sql
+++ vape_seller.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2025 at 12:29 AM
+-- Generation Time: May 26, 2025 at 08:59 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_logs`
+--
+
+CREATE TABLE `tbl_logs` (
+  `logs_id` int(30) NOT NULL,
+  `u_id` int(30) NOT NULL,
+  `action` varchar(30) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_orders`
 --
 
@@ -35,6 +48,13 @@ CREATE TABLE `tbl_orders` (
   `date` date NOT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_orders`
+--
+
+INSERT INTO `tbl_orders` (`o_id`, `u_id`, `v_id`, `quantity`, `date`, `status`) VALUES
+(1, 30, 1003, '10', '2025-05-26', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -50,6 +70,8 @@ CREATE TABLE `tbl_user` (
   `u_username` varchar(50) NOT NULL,
   `u_type` varchar(50) NOT NULL,
   `u_password` varchar(50) NOT NULL,
+  `u_question` varchar(100) NOT NULL,
+  `u_answer` varchar(100) NOT NULL,
   `u_contact` varchar(50) NOT NULL,
   `u_status` varchar(50) NOT NULL,
   `u_image` varchar(50) NOT NULL
@@ -59,14 +81,17 @@ CREATE TABLE `tbl_user` (
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_username`, `u_type`, `u_password`, `u_contact`, `u_status`, `u_image`) VALUES
-(30, 'tests', 'test', 'test@gmail.com', 'test', 'User', 'NyaDNd1pMQRb3N+SYj/4GaZCRLU9DnRtQ4eXNJ1NpXg=', '09234990256', 'Active', ''),
-(32, 'tests', 'tests', 'tests23@gmail.com', 'tests', 'Admin', 'g3Vu6+bbccSZxJizaJwsIWa0bLRje5FE013FpERZ8aM=', '09452341711', 'Active', ''),
-(33, 'testing', 'testing', 'testing@gmail.com', 'testing', 'User', '73l8gRjwLftklgfdXT+MdiMEjJwGPVMsyVxe16iYpk8=', '09234990256', 'Active', ''),
-(34, 'mels', 'limpo', 'mellimpo@gmail.com', 'mel', 'Admin', 'rQeIGQ5aX7n+n0PB5KDwZT9lCR3xGT3N2a+JOhbSitI=', '09622496073', 'Active', ''),
-(35, 'alfred', 'villena', 'alfred23@gmail.com', 'alfred123', 'Admin', 'ky88G1YlfOhTmsJp16q0JVDaz4gY0HXwvfGZBWKq4+8=', '09234990256', 'Active', ''),
-(36, 'jenny', 'billones', 'jenny@gmail.com', 'jenny1', 'Admin', 'mRh+H9R+Yh4EvRy70GC3efjAbAuGgN0lNnEfLpWJbck=', '09234990256', 'Active', 'src/userimages/adminpp_edit-removebg-preview.png'),
-(37, 'tralalelo', 'test', 'hahah@gmail.com', 'tralalelo', 'Admin', 'ky88G1YlfOhTmsJp16q0JVDaz4gY0HXwvfGZBWKq4+8=', '09234990556', 'Active', 'src/userimages/Mari.png');
+INSERT INTO `tbl_user` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_username`, `u_type`, `u_password`, `u_question`, `u_answer`, `u_contact`, `u_status`, `u_image`) VALUES
+(30, 'tests', 'test', 'test@gmail.com', 'test', 'User', 'NyaDNd1pMQRb3N+SYj/4GaZCRLU9DnRtQ4eXNJ1NpXg=', '', '', '09234990256', 'Active', ''),
+(32, 'tests', 'tests', 'tests23@gmail.com', 'tests', 'Admin', 'g3Vu6+bbccSZxJizaJwsIWa0bLRje5FE013FpERZ8aM=', '', '', '09452341711', 'Active', ''),
+(33, 'testing', 'testing', 'testing@gmail.com', 'testing', 'User', '73l8gRjwLftklgfdXT+MdiMEjJwGPVMsyVxe16iYpk8=', '', '', '09234990256', 'Active', ''),
+(34, 'mels', 'limpo', 'mellimpo@gmail.com', 'mel', 'Admin', 'rQeIGQ5aX7n+n0PB5KDwZT9lCR3xGT3N2a+JOhbSitI=', '', '', '09622496073', 'Active', ''),
+(35, 'alfred', 'villena', 'alfred23@gmail.com', 'alfred123', 'Admin', 'ky88G1YlfOhTmsJp16q0JVDaz4gY0HXwvfGZBWKq4+8=', '', '', '09234990256', 'Active', ''),
+(36, 'jenny', 'billones', 'jenny@gmail.com', 'jenny1', 'Admin', 'mRh+H9R+Yh4EvRy70GC3efjAbAuGgN0lNnEfLpWJbck=', '', '', '09234990256', 'Active', 'src/userimages/adminpp_edit-removebg-preview.png'),
+(37, 'tralalelo', 'test', 'hahah@gmail.com', 'tralalelo', 'Admin', 'ky88G1YlfOhTmsJp16q0JVDaz4gY0HXwvfGZBWKq4+8=', '', '', '09234990556', 'Active', 'src/userimages/Mari.png'),
+(38, 'awdawdawda', 'ssssawdawdawd', 'awdawdawd@gmail.vom', 'tester', 'Admin', '2xY/4ALk+Or1Y2gxGqwbSd5bLrdddSLt2TuK5h5iotQ=', 'Undecided', 'Who is your first love?', '09234990256', 'Active', 'q61I74hjoyI7xVplzXKDQKARWI6odce9/rfsRyd5x4Q='),
+(39, 'awdawd', 'awdawd', 'awdawd@gmail.com', 'awdawd', 'Admin', 'ky88G1YlfOhTmsJp16q0JVDaz4gY0HXwvfGZBWKq4+8=', 'Undecided', 'Where were you born?', '092341990265', 'Pending', 'VSNdEMlcGce7ff0/nyy6wYS4ZirjawCaJgBQcLgLcX8='),
+(40, 'clyde', 'kayat', 'bata@gmail.com', 'testers', 'Admin', 'ky88G1YlfOhTmsJp16q0JVDaz4gY0HXwvfGZBWKq4+8=', 'Who is your first love?', '7Glqnjy6nd37s9C6rit+YjnrlkYz2Sj5gocOezj6+KI=', '09234990256', 'Active', 'Undecided');
 
 -- --------------------------------------------------------
 
@@ -98,6 +123,13 @@ INSERT INTO `tbl_vapes` (`v_id`, `v_name`, `v_price`, `v_status`, `v_image`) VAL
 --
 
 --
+-- Indexes for table `tbl_logs`
+--
+ALTER TABLE `tbl_logs`
+  ADD PRIMARY KEY (`logs_id`),
+  ADD KEY `u_id` (`u_id`);
+
+--
 -- Indexes for table `tbl_orders`
 --
 ALTER TABLE `tbl_orders`
@@ -122,16 +154,22 @@ ALTER TABLE `tbl_vapes`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_logs`
+--
+ALTER TABLE `tbl_logs`
+  MODIFY `logs_id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tbl_orders`
 --
 ALTER TABLE `tbl_orders`
-  MODIFY `o_id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `o_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `tbl_vapes`
@@ -142,6 +180,12 @@ ALTER TABLE `tbl_vapes`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tbl_logs`
+--
+ALTER TABLE `tbl_logs`
+  ADD CONSTRAINT `u_id` FOREIGN KEY (`logs_id`) REFERENCES `tbl_user` (`u_id`);
 
 --
 -- Constraints for table `tbl_orders`
